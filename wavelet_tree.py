53c9aeb73bb95c0d739badc2c8a5090a236f6152
Allegris <AstridChristiansenCS@gmail.com>
2022-11-21 12:10:07 +0100

    Cleanup in WT code

--- wavelet_tree.py
+++ wavelet_tree.py
@@ -10,8 +10,7 @@ class WaveletTree:
 		self.root = WaveletTreeNode(x, 0, None)
 
 	'''
-	Iterates a wavelet tree, starting from the root, and returns the rank of a
-	given char c and a given index i.
+	Rank query for a given char c and a given index i.
 	'''
 	def rank(self, c, i):
 		# Current node and rank
@@ -26,8 +25,6 @@ class WaveletTree:
 
 
 
-
-
 ########################################################
 # Class for wavelet tree internal nodes
 ########################################################
@@ -36,11 +33,11 @@ class WaveletTreeNode:
 	def __init__(self, s, level, root):
 		# Root pointer and store codes at root
 		if level == 0:
-			self.root = self
 			self.codes = huffman_codes(s)
+			self.root = self
 		else:
 			self.root = root
-		# Construct bitvector and split s in two parts (child nodes)
+		# Construct node bitvector and split s in two parts (child nodes)
 		self.bitvector, s0, s1 = split_node(s, self.root.codes, level)
 		# Preprocess ranks
 		self.word_ranks = preprocess_node_word_ranks(self.bitvector)
@@ -53,36 +50,15 @@ class WaveletTreeNode:
 		if alphabet_size(s1) > 1:
 			self.right_child = WaveletTreeNode(s1, level+1, self.root)
 
-########################################################
-# Rank query using wavelet tree
-########################################################
 
-
-'''
-Iterates a wavelet tree, starting from the root, and returns the rank of a
-given char c and a given index i.
-
-def rank(root, c, i):
-	# Current node and rank
-	node = root
-	rank = i
-	# Iterate chars in code
-	for char in root.codes[c]:
-		# Update rank and node
-		rank = bitvector_rank(node.bitvector, node.word_ranks[char], char, rank)
-		node = node.right_child if char else node.left_child
-	return rank
-'''
 
 ########################################################
 # Code to run
 ########################################################
-'''
-x = "mississippi"
-root = WaveletTreeNode(x, 0, None) # x, level, root
-print(rank(root, "m", 0))
 
-'''
+x = "mississippi"
+wt = WaveletTree(x)
+print(wt.rank("m", 2))
 
 
 '''
