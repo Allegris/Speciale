174ec1c77035bfd2325aa1c4b9ccf04105c58bf5
Allegris <AstridChristiansenCS@gmail.com>
2022-11-21 12:30:25 +0100

    Done with WT

--- wavelet_tree.py
+++ wavelet_tree.py
@@ -7,7 +7,8 @@ from shared import alphabet_size, bitvector_rank, preprocess_node_word_ranks, sp
 
 class WaveletTree:
 	def __init__(self, x):
-		self.root = WaveletTreeNode(x, 0, None)
+		self.codes = huffman_codes(x)
+		self.root = WaveletTreeNode(x, 0, self.codes)
 
 	'''
 	Rank query for a given char c and a given index i.
@@ -17,7 +18,7 @@ class WaveletTree:
 		node = self.root
 		rank = i
 		# Iterate chars in code
-		for char in self.root.codes[c]:
+		for char in self.codes[c]:
 			# Update rank and node
 			rank = bitvector_rank(node.bitvector, node.word_ranks[char], char, rank)
 			node = node.right_child if char else node.left_child
@@ -30,37 +31,36 @@ class WaveletTree:
 ########################################################
 
 class WaveletTreeNode:
-	def __init__(self, s, level, root):
-		# Root pointer and store codes at root
-		if level == 0:
-			self.codes = huffman_codes(s)
-			self.root = self
-		else:
-			self.root = root
+	def __init__(self, s, level, codes):
 		# Construct node bitvector and split s in two parts (child nodes)
-		self.bitvector, s0, s1 = split_node(s, self.root.codes, level)
+		self.bitvector, s0, s1 = split_node(s, codes, level)
 		# Preprocess ranks
 		self.word_ranks = preprocess_node_word_ranks(self.bitvector)
 		# Create children
 		self.left_child, self.right_child = None, None
 		# If left child is an inner node
 		if alphabet_size(s0) > 1:
-			self.left_child = WaveletTreeNode(s0, level+1, self.root)
+			self.left_child = WaveletTreeNode(s0, level+1, codes)
 		# If right child is an inner node
 		if alphabet_size(s1) > 1:
-			self.right_child = WaveletTreeNode(s1, level+1, self.root)
+			self.right_child = WaveletTreeNode(s1, level+1, codes)
 
 
 
 ########################################################
 # Code to run
 ########################################################
-
+'''
 x = "mississippi"
 wt = WaveletTree(x)
 print(wt.rank("m", 2))
+'''
 
 
+
+########################################################
+# For report
+########################################################
 '''
 # For report (simplified version)
 def rank(root, codes, c, i):
